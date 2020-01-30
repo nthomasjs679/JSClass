b6bb7045301d7ebdca0809c69c0898ede618bebc
nthomasjs679 <nthomasjs679@gmail.com>
2020-01-30 12:28:31 -0600

    Created Simple Website - all resources

---
+++ strings.js
@@ -0,0 +1,9 @@
+function Click(){
+    document.getElementById("demo").innerHTML = "Congratulations!"
+alert("Congratulations! You can read directions!");
+}
+
+function Ignore(){
+    document.getElementById("demo").innerHTML = "You Fail!"
+    alert("You Fail!");
+}
