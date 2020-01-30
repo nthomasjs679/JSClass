ed82870e291cc3a4f513e27903057642d7d3fcf7
nthomasjs679 <nthomasjs679@gmail.com>
2020-01-30 14:30:43 -0600

    Improved Name Submission

--- strings.js
+++ strings.js
@@ -7,3 +7,7 @@ function Ignore(){
     document.getElementById("demo").innerHTML = "You Fail!"
     alert("You Fail!");
 }
+function Submit(){
+    var enteredName = document.getElementById("nameEntered").value; 
+    document.getElementById("name").innerHTML = enteredName;
+}
